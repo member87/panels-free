a26906259272b2e4acc59e1338ec63dfbfc8e5d3
member87 <43416057+member87@users.noreply.github.com>
2024-09-27 17:17:38 +0100

    added github link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 import ImageList from "@/components/ImageList";
-import Image from "next/image";
 
 export default async function Home() {
   const { media, content } = await getData();
 
   return (
     <div className="p-4">
+      <div className="text-center mb-4">
+        <h1 className="text-4xl font-bold text-center">Panels</h1>
+        <span className="text-2xl text-gray-100">Source code available on <a href="https://github.com/member87/panels-free" className="text-blue-400 underline">GitHub</a></span>
+      </div>
       <ImageList data={media} artists={content} />
     </div>
   );
 }
-
 
 const getData = async () => {
   const url = "https://storage.googleapis.com/panels-api/data/20240916/spec.json";
